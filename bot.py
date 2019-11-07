bc3a69d63133531be7b566a5c0d2431791b30adb
Javen Zamojcin <javen.william@gmail.com>
2019-11-07 14:05:45 -0500

    Added check-in feature

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
 # bot.py
 import os
+import time
+from random import randint
+
 import discord
 from dotenv import load_dotenv
 from discord.ext import commands
+
+from check_in_config import CHECK_IN_QUESTIONS_FIRST_YEARS, CHECK_IN_QUESTIONS_RETURNERS, CHECK_IN_HEADER_MESSAGE,\
+    CHECK_IN_NUMBER
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -12,9 +18,14 @@ COMMAND_PREFIX = os.getenv('COMMAND_PREFIX')
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
 
-def find_guild():
-    guild = discord.utils.get(bot.guilds, name=GUILD)
-    return guild
+# Listening variables. If enabled, on_message will redirect message to the listener's respective function.
+listening_student_year_user_list = []
+listening_check_in_data_user_list = []
+
+
+#########################################
+# Events
+#########################################
 
 
 @bot.event
@@ -32,7 +43,42 @@ async def on_member_join(member):
 async def on_message(message):
     if message.author == bot.user:
         return
+
+    global listening_student_year_user_list
+    global listening_check_in_data_user_list
+
+    # disable listeners
+    if message.content == 'ra done':
+        if message.author in listening_student_year_user_list:
+            listening_student_year_user_list.remove(message.author)
+        if message.author in listening_check_in_data_user_list:
+            listening_check_in_data_user_list.remove(message.author)
+        await message.author.create_dm()
+        await message.author.dm_channel.send('Thank you!')
+        return
+
+    # if bot is in listening for student year mode for student
+    if message.author in listening_student_year_user_list:
+        if str(message.channel) == f'Direct Message with {message.author}':
+            await command_check_in_message(message)
+
+    # if bot is in listening for check-in data mode for student and is in a DM channel
+    if message.author in listening_check_in_data_user_list:
+        if str(message.channel) == f'Direct Message with {message.author}':
+            return write_check_in(message)
+
     await bot.process_commands(message)
+
+    if str(message.channel) == f'Direct Message with {message.author}':
+        if message.author not in listening_check_in_data_user_list:
+            if message.author not in listening_student_year_user_list:
+                await message.author.create_dm()
+                await message.author.dm_channel.send('You are not in check-in mode. Type "ra check-in" to begin.')
+
+
+###########################################################
+# Commands
+###########################################################
 
 
 @bot.command(name='hello')
@@ -47,11 +93,91 @@ async def command_hehwo(ctx):
     await ctx.send(response)
 
 
-@bot.command(name='server_members')
+@bot.command(name='king')
+async def command_king(ctx):
+    response = 'He who possesses the most tide pods, holds the most respect. '
+    await ctx.send(response)
+
+
+@bot.command(name='check-in')
+async def command_check_in_ctx(ctx):
+    global listening_student_year_user_list
+    if ctx.message.author in listening_student_year_user_list:
+        return
+    print(f'\n{ctx.message.author} has started their check-in.')
+    await command_check_in_message(ctx.message)
+
+
+async def command_check_in_message(message):
+    await message.author.create_dm()
+    global listening_student_year_user_list
+    global listening_check_in_data_user_list
+
+    if message.author in listening_check_in_data_user_list:
+        return
+    if message.author not in listening_student_year_user_list:
+        await message.author.dm_channel.send(CHECK_IN_HEADER_MESSAGE)
+        await message.author.dm_channel.send('-')
+    else:
+        listening_student_year_user_list.remove(message.author)
+
+    response = ''
+    if message.content == 'first-year':
+        for question in CHECK_IN_QUESTIONS_FIRST_YEARS:
+            response += question
+            if message.author not in listening_check_in_data_user_list:
+                listening_check_in_data_user_list.append(message.author)
+    elif message.content == 'returner':
+        for question in CHECK_IN_QUESTIONS_RETURNERS:
+            response += question
+            if message.author not in listening_check_in_data_user_list:
+                listening_check_in_data_user_list.append(message.author)
+    else:
+        response = 'Are you a first-year or returner?'
+        if message.author not in listening_student_year_user_list:
+            listening_student_year_user_list.append(message.author)
+
+    await message.author.dm_channel.send(response)
+
+
+@bot.command(name="trivia")
+async def command_trivia(ctx):
+    response = 'pls trivia'
+    await ctx.send(response)
+    answer = randint(1, 4)
+    time.sleep(2)
+    await ctx.send(answer)
+
+
+@bot.command(name='oops')
+async def command_oops(ctx):
+    response = 'OOPSIE WOOPSIE!! Uwu We made a fucky wucky!! A wittle fucko boingo!' \
+              ' The code monkeys at our headquarters are working VEWY HAWD to fix this!'
+    await ctx.send(response)
+
+
+@bot.command(name='list-members')
 async def print_server_members(ctx):
     members = '\n - '.join([member.name for member in find_guild().members])
     response = f'{GUILD}:\n - {members}'
     await ctx.send(response)
 
 
+###################################################
+# Helper Functions
+###################################################
+
+
+def find_guild():
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    return guild
+
+
+def write_check_in(message):
+    file = open(f'check-ins/{CHECK_IN_NUMBER}-{message.author}.txt', 'a')
+    file.write(message.content + '\n')
+    file.close()
+
+
+# Run Script
 bot.run(TOKEN)
---
+++ check_in_config.py
@@ -0,0 +1,45 @@
+# Which check-in are these questions for?
+CHECK_IN_NUMBER = 2
+
+# The message that will be displayed before the questions are asked.
+CHECK_IN_HEADER_MESSAGE = '\nHello!\nThe following questions are not a quiz, more so a guideline. Feel free to talk ' \
+                          'about ' \
+                          'whatever you what, as long as you can provide a decent idea of how you are doing. Note ' \
+                          'that your responses will be recorded until you type the command "ra done". \n\n '
+
+# Check-in questions for returning students.
+CHECK_IN_QUESTIONS_FIRST_YEARS = [
+    'How are you feeling about your classes?' + '\n',
+    'How are things going with your roommate, hall and friends?' + '\n',
+    'When you need help with your coursework, how do you reach out?' + '\n',
+    'What resources on campus are you using to help prepare you?' + '\n',
+    'How often do you visit your professor’s office hours?' + '\n',
+    'Are there any professors you find are very helpful?' + '\n',
+    'If you aren’t stressed now, do you anticipate classes, clubs, or work getting more stressful? How might you '
+    'manage this?' + '\n',
+    'What do you do when you feel that you need help?' + '\n',
+    'Is there anything I can help you with?' + '\n',
+    'How often do you advocate for yourself?' + '\n',
+    'What strategies do you use to make yourself feel heard?' + '\n',
+    'Are there any people that you feel make it more difficult to advocate for your needs?' + '\n',
+    '\nIf you are stressed:' + '\n',
+    'What has been stressing you out?' + '\n',
+    'Is there anything I can do to help or do you have someone you can talk to about this?' + '\n',
+    'What kinds of things are you doing to manage that stress?' + '\n',
+    'What kinds of things do you tend to do to manage your stress when you’ve felt stressed out in the past?' + '\n',
+]
+
+# Check-in questions for first year students.
+CHECK_IN_QUESTIONS_RETURNERS = [
+    'How are you feeling about your classes?' + '\n',
+    'How are you preparing for finals?' + '\n',
+    'How are things going with your roommate/house/friends?' + '\n',
+    'How do you think you have changed over the semester?' + '\n',
+    'What clubs or organizations have you joined?' + '\n',
+    'Do you have the support network that you want?' + '\n',
+    'Do you feel the people around you are positively or negatively impacting you? How?' + '\n',
+    'Which professors or staff members do you think are the most influential to you? Why?' + '\n',
+    'How do you think you are influencing other people?' + '\n',
+    'How do you want people to perceive you?' + '\n',
+    'How do you think others perceive you? Is it positive or negative?' + '\n',
+]
